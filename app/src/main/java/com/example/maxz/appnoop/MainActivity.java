ac465dece97ea9eef44d2f0ef00d823481faddae
Teerasak <maxz_drum@icloud.com>
2016-09-21 01:08:19 +0700

    แก้ช่องว่างแล้ว หน้า mainActivity

--- app/src/main/java/com/example/maxz/appnoop/MainActivity.java
+++ app/src/main/java/com/example/maxz/appnoop/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.maxz.appnoop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 
@@ -17,7 +18,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         nameEditText = (EditText) findViewById(R.id.editTextName);
+        passwordEditText = (EditText) findViewById(R.id.editTextPassword);
 
 
     }//main method
+
+    public void cicksigninActivity(View view) {
+
+        nameString = nameEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (nameString.equals("") || passwordString.equals("")) {
+            MyDialog myDialog = new MyDialog();
+            myDialog.myAler(this,"มีช่องว่าง กรุณากรอกให้ครบนะจ๊ะ","ผิดพลาด");
+        }
+
+    }
 }//main class
